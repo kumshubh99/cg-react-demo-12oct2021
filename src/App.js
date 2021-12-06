8b02f08d210b91819a1ac6fb53b5038e13c284f1
kumshubh99 <93115869+kumshubh99@users.noreply.github.com>
2021-12-06 17:56:44 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 
+// import { useEffect } from "react";
 import EmpData from "./components/EmpData";
 import Header from "./components/Header";
 
 const App = () => {
+  console.log('App');
   const num = 10;
+  const myData = { abc: 15, def: 25 }
+  const myArray = [10, 20, 30];
+
+  let dataFromChild = 0;
+
+  // useEffect(() => {
+  //   dataFromChild = 45;
+  // }, []);
+
+  const parentFun = (arg) => {
+    console.log('parentFun');
+    dataFromChild = arg(); // callback
+    console.log(dataFromChild);
+  }
+
+  // const aNormalFunction = () => {
+  //   console.log('normal funciton');
+  // }
 
   return (
     <div>
       <Header />
       <h1>CG React App</h1>
+      <p> {dataFromChild} </p>
+
+      {/* This will not work */}
+      {/* <p>{myData} </p> */}
+      {/* This will work  */}
+      <p>{myData.abc} </p>
+      <p>{myData.def} </p>
+      <p>{myArray}</p>
+      <p>{myArray[0]}</p>
       <p>{num}</p>
-      <EmpData numToPassToChild={num} />
+      <EmpData
+        // normalFun={aNormalFunction}
+        callParentFun={parentFun}
+        someData={myData}
+        numToPassToChild={num}
+        anotherData={myData}
+      />
     </div>
   );
 }
@@ -52,6 +87,3 @@ export default App;
 //   );
 // }
 // export default App;
-
-
-
--- src/components/EmpData.js
+++ src/components/EmpData.js
@@ -3,12 +3,31 @@
 // 1. from parent component to child component - props 
 // 2. from child compoment to parent component - ??
 
-
 const EmpData = (props) => {
+    console.log('EmpData')
+
+    const childData = 2211;
+
+    // props.normalFun();
+
+
+    const childFun = () => {
+        console.log('childFun');
+        return childData;
+    }
+
     return (
         <div>
             <h6>Emp Data Component</h6>
+            <p>{props.someData.def}</p>
             <p>{props.numToPassToChild}</p>
+            {/* try this  */}
+            {/* <p>{props.anotherData}</p> */}
+            <p>{props.someOtherData}</p>
+            {/* Invoke function from parent  */}
+            {/* <p>{props.callParentFun}</p> */}
+            <button onClick={() => { props.callParentFun(childFun) }} >
+                Pass data From child To parent</button>
         </div>
     );
 }
